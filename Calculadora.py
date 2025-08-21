aee9e340b83cfbeca350b4deed4ea7962dbf7b24
Guigo Costa <118695299+gscosta19@users.noreply.github.com>
2025-08-20 21:06:20 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+# Calculadora
+a=int(input("Digite o primeiro número: "))
+b=int(input("Digite o segundo número: "))
+
+soma = a+b
+sub = a-b
+mult = a*b
+div = a/b
+
+print("O valor da soma entre {} e {} é igual a {}".format(a,b,soma))
+print("A subtração de {} e {} vai ser {}".format(a,b,sub))
+print("Se eu pegar {} e dividir por {} o resultado será {}".format(a,b,div))
+print("Multiplicação: {} * {} = {}".format(a,b,mult))
+print(f"teste para {a} e {b}")
